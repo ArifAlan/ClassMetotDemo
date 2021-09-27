c5a560b6d6804b17db84184076d8d702bff76f77
Arif Alan <arifalan001@hotmail.com>
2021-09-27 19:54:04 +0300

    .

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle()
+        {
+            Console.WriteLine("Tebrikler müşteri başarıyla kaydedildi.");
+        }
+        public void Silme()
+        {
+            Console.WriteLine("Tebrikler müşteri başarıyla silindi.");
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri kullanıcı1 = new Musteri();
+            kullanıcı1.Id = 1;
+            kullanıcı1.Adi = "Engin Demiroğ";
+            kullanıcı1.Yasi = 33;
+            kullanıcı1.Sehri = "Diyarbakır";
+
+            Musteri kullanıcı2 = new Musteri();
+            kullanıcı2.Id = 2;
+            kullanıcı2.Adi = "Kaan Gök";
+            kullanıcı2.Yasi = 24;
+            kullanıcı2.Sehri = "Gaziantep";
+
+            Musteri[] kullancılar = new Musteri[] {kullanıcı1, kullanıcı2};
+
+            foreach (var musteri in kullancılar) 
+            {
+                Console.WriteLine("Müşteri Id : "+musteri.Id);
+                Console.WriteLine("Müşteri Adı : " + musteri.Adi);
+                Console.WriteLine("Müşteri yaşı : " + musteri.Yasi);
+                Console.WriteLine("Müşteri şehri : " + musteri.Sehri);
+
+                Console.WriteLine("                ");
+
+            }
+            
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle();
+            musteriManager.Silme();
+
+          
+        }
+    }
+}
